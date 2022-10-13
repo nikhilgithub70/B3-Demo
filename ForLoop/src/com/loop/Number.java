eb539eb740370e74f12eb8720fd472ace2d88dff
nikhil <patilnr70@gmail.com>
2022-10-13 18:43:03 +0530

    for loop program

---
+++ ForLoop/src/com/loop/Number.java
@@ -0,0 +1,15 @@
+package com.loop;
+
+public class Number {
+	
+	public static void main(String[] args) {
+		
+		int a=0;
+		
+		for (int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+			
+	}
+
+}
